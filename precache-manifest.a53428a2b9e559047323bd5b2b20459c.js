e98dfb71707cd28bff86f97947fef9bb2475c696
Will <willian.maziero@hotmail.com>
2023-05-18 19:39:48 -0400

    Updates

---
+++ precache-manifest.a53428a2b9e559047323bd5b2b20459c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d108805b4e66038c0e08",
+    "url": "/willianmaziero/static/css/main.7763f605.chunk.css"
+  },
+  {
+    "revision": "d108805b4e66038c0e08",
+    "url": "/willianmaziero/static/js/main.d108805b.chunk.js"
+  },
+  {
+    "revision": "62f89b0737526706d4ea",
+    "url": "/willianmaziero/static/js/1.62f89b07.chunk.js"
+  },
+  {
+    "revision": "b4be688e650068c8904a",
+    "url": "/willianmaziero/static/js/runtime~main.b4be688e.js"
+  },
+  {
+    "revision": "22d5b832a7c5ed2c54067c4c9da4f1ce",
+    "url": "/willianmaziero/static/media/profile.22d5b832.png"
+  },
+  {
+    "revision": "3872bbf1f1aa5690b5a8cad4acf5e534",
+    "url": "/willianmaziero/static/media/splashDogs.3872bbf1.png"
+  },
+  {
+    "revision": "868b56e01a52c8ac4a3f447baef02fab",
+    "url": "/willianmaziero/static/media/splashOrbiout.868b56e0.png"
+  },
+  {
+    "revision": "e435971af0d6dc17e79837d5e6fe0cc7",
+    "url": "/willianmaziero/static/media/logo.e435971a.svg"
+  },
+  {
+    "revision": "098a8f04cb63f203f6a1e0e64264022e",
+    "url": "/willianmaziero/static/media/arrowCorner.098a8f04.svg"
+  },
+  {
+    "revision": "775fedca720ea9f6d9a5efa8273e3650",
+    "url": "/willianmaziero/static/media/person.775fedca.svg"
+  },
+  {
+    "revision": "b390ead44a315b684b6c9c57502ba89a",
+    "url": "/willianmaziero/static/media/location.b390ead4.svg"
+  },
+  {
+    "revision": "7a4e6fecc51ebd69bdf54a5c9af4b467",
+    "url": "/willianmaziero/static/media/email.7a4e6fec.svg"
+  },
+  {
+    "revision": "27996f85fc9bca73027e3f613cfb2aaa",
+    "url": "/willianmaziero/static/media/github-1.27996f85.svg"
+  },
+  {
+    "revision": "99b5346c9d9acb15f5433d3ead6538af",
+    "url": "/willianmaziero/static/media/linkedin-3.99b5346c.svg"
+  },
+  {
+    "revision": "83fc04bb506435057c7d0b6b49fce812",
+    "url": "/willianmaziero/index.html"
+  }
+];
